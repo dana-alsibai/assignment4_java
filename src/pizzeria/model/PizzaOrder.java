55c5091a4ee5e3b668fe7fcb049cf3e12e635978
dana-alsibai <alsibaid@sheridancollege.ca>
2020-03-19 20:03:24 -0400

    branch_4

---
+++ src/pizzeria/model/PizzaOrder.java
@@ -0,0 +1,19 @@
+  /*
+        Name:  [Dana Alsibai ]
+        Assignment:  [Exercise 1.1a]
+        Program: []
+        Date:  [assignment due date here]
+    
+        Description:
+        [program description in your own words]
+    */
+
+package pizzeria.model;
+
+/**
+ *
+ * @author danasebai
+ */
+public class PizzaOrder {
+
+}
